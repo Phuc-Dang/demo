85d4f71911c0cd5aa2b7b0de7b2da43bfe115839
bi151103 <93708621+bi151103@users.noreply.github.com>
2023-02-11 22:13:36 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include <list>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    
+    return 0;
+}
